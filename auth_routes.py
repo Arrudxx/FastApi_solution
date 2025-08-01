2f3fcdaffed1fc7cce1d4318953ad28f58c1adba
Arrudxx <d4niel.arruda@gmail.com>
2025-07-31 21:14:02 -0300

    criação de criptografia de senha para criação de usuário

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from models import Usuario
 from dependencies import pegar_sessao
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix='/auth', tags=['Auth'])
 
@@ -18,7 +19,8 @@ async def criar_conta(email: str, senha:str, nome: str, session = Depends(pegar_
         #Já existe um usuario com este email
         return {'mensagem': 'já existe um usuário com este email'}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {'mensagem': 'usuario cadastrado com sucesso'}
